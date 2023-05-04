d9e54c2f8a27c52919e01298ac5046670cba6cfc
valera_kudryavtsev <shrhannor@gmail.com>
2023-05-05 01:02:31 +0300

    Add auth-token support + follow team button

--- src/components/Leagues/Standings.js
+++ src/components/Leagues/Standings.js
@@ -3,6 +3,8 @@ import {useEffect, useState} from "react";
 import ErrorPage from "../Other/ErrorPage";
 import Loader from "../Other/Loader";
 import Team from "./Team";
+import Image from "../Other/Image";
+import logo from '../../images/logo.png'
 
 const Standings = () => {
     const {code} = useParams()
@@ -14,11 +16,7 @@ const Standings = () => {
     useEffect(() => {
         setLoading(true)
         setTimeout(() => {
-            fetch(`https://api.football-data.org/v4/competitions/${code}/standings`, {
-                headers: {
-                    'X-Auth-Token': 'fe382c8a16f0430ca32f601d38888099'
-                },
-            })
+            fetch(`http://localhost:8080/standings/${code}`)
                 .then(resp => resp.json())
                 .then(data => {
                     setData(data.standings[0].table)
@@ -47,7 +45,10 @@ const Standings = () => {
                             <div className="standings__titles-box">
                                 <div className="standings__titles">
                                     <p className="team__position">#</p>
-                                    <p className="team__title-name">КОМАНДА</p>
+                                    <Image src={logo} alt="team logo" className="team__emblem" />
+                                    <p className="team__name">
+                                        КОМАНДА
+                                    </p>
                                     <ul className="team-stats__list">
                                         <li className="team-stats__item">И</li>
                                         <li className="team-stats__item">В</li>
@@ -59,6 +60,12 @@ const Standings = () => {
                                         <li className="team-stats__item team-stats__item-bold">О</li>
                                     </ul>
                                     <div className="team__form">ФОРМА</div>
+                                    <div className="team__follow-button">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" className="team__follow-pic-h" viewBox="0 0 16 16">
+                                            <path
+                                                d="M2 15.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v13.5zM8 4.41c1.387-1.425 4.854 1.07 0 4.277C3.146 5.48 6.613 2.986 8 4.412z"/>
+                                        </svg>
+                                    </div>
                                 </div>
                             </div>
                             {data.map((e, id) => <Team team={e} key={id} />)}
