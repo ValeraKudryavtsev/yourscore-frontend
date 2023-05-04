bb4a37b8202e9bdcdac9b3f50300e3956ee272fd
valera_kudryavtsev <shrhannor@gmail.com>
2023-05-05 01:02:18 +0300

    Add auth-token support + follow team button

--- src/components/Leagues/Team.js
+++ src/components/Leagues/Team.js
@@ -4,6 +4,28 @@ const Team = (props) => {
     const {team} = props
     const form = team.form.split(',')
 
+    function followTeam() {
+        fetch("http://localhost:8080/team/follow", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "AUTHORIZATION": window.localStorage.getItem('token')
+            },
+            body: JSON.stringify({
+                teamCode: team.team.id,
+                teamName: team.team.name,
+                teamEmblemUrl: team.team.crest
+            })
+        })
+            .then(resp => resp.text())
+            .then(data => {
+                console.log(data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
     return (
         <div className="team">
             <p className="team__position">
@@ -31,6 +53,12 @@ const Team = (props) => {
                     )
                 )}
             </ul>
+            <button className="team__follow-button" onClick={followTeam}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" className="team__follow-pic" viewBox="0 0 16 16">
+                    <path
+                        d="M2 15.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2v13.5zM8 4.41c1.387-1.425 4.854 1.07 0 4.277C3.146 5.48 6.613 2.986 8 4.412z"/>
+                </svg>
+            </button>
         </div>
     )
 }
